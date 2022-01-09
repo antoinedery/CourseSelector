82506d1fee0bbc04c44234ae8224a5262bca8bdc
Antoine <antoinedery12@hotmail.fr>
2022-01-09 08:55:19 -0500

    program can be periodically launched until course is added

--- Main.py
+++ Main.py
@@ -1,12 +1,18 @@
 import pwinput
+import time
+from CourseAdder import addCourse, logIntoStudentAccount, openBrowser
 
 username = input("Enter your username : ")
 password = pwinput.pwinput(prompt="Enter your password : ")
-dob =  pwinput.pwinput(prompt="Enter your date of birth (YYYYMMDD or YYMMDD) : ")
+dob = pwinput.pwinput(prompt="Enter your date of birth (YYYYMMDD or YYMMDD) : ")
 courseNumber = input("Enter the course name : ")
 thGroup = input("Enter the theoretical group number : ")
 labGroup = input("Enter the lab group number : ")
 
-from CourseAdder import addCourse, logIntoStudentAccount
-logIntoStudentAccount(username, password, dob)
-print(addCourse(courseNumber, thGroup, labGroup))
+isCourseAdded = False
+while not(isCourseAdded):
+    print("Program is running...")
+    openBrowser()
+    logIntoStudentAccount(username, password, dob)
+    addCourse(courseNumber, thGroup, labGroup)
+    time.sleep(60)
