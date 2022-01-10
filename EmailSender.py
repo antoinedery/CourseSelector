e7e183b7de5e73e09287cd316bb61a5e7fb9b5d1
Antoine <antoinedery12@hotmail.fr>
2022-01-10 15:31:28 -0500

    email format improved

--- EmailSender.py
+++ EmailSender.py
@@ -1,15 +1,24 @@
-import smtplib, ssl
+import smtplib
+import ssl
 
 port = 465
 smtp_server = "smtp.gmail.com"
 sender_email = "PolyScheduleModifier@gmail.com"
-userEmailAddress = "antoinedery12@hotmail.fr"
 password = input("Type your password and press enter: ")
 
-def sendEmail(courseNumber, userEmailAddress) : 
+def sendEmail(courseNumber, thGroup, labGroup, userEmailAddress):
     message = """\
-    Subject: PolyScheduleModifier - Your course was added
-    This message is to inform you that the course {courseNumber} was added to your schedule"""
+    Subject:""" + courseNumber + """ was added to your schedule - PolyScheduleModifier
+
+    This message is to inform you that the course """ + courseNumber + """ was added to your schedule in the following groups: 
+    Theoretical group: """ + thGroup + """
+    Laboratory group: """ + labGroup + """
+
+    Thank you for using PolyScheduleModifier!
+
+    Antoine
+    """
+    
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
         server.login(sender_email, password)
