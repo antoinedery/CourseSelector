a440d93e8e4c319b25f35e1d5fa9bb6054281fa5
Antoine Déry <antoinedery12@hotmail.fr>
2022-03-01 19:32:36 -0500

    course choice done, tooltip and snackbar addition

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 
 @NgModule({
   declarations: [AppComponent, HomePageComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, MatIconModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    MatIconModule,
+    FormsModule,
+    MatSnackBarModule,
+    BrowserAnimationsModule,
+    MatTooltipModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- client/src/app/components/home-page/home-page.component.ts
+++ client/src/app/components/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { User } from '@app/classes/user';
 import { ClientSocketService } from '@app/services/client-socket.service';
 
@@ -12,17 +13,23 @@ export class HomePageComponent {
   @ViewChild('passwordInput') passwordInput!: ElementRef;
   @ViewChild('dobInput') dobInput!: ElementRef;
   @ViewChild('emailInput') emailInput!: ElementRef;
-  @ViewChild('coursesInput') coursesInput!: ElementRef;
   @ViewChild('passwordVisibility') passwordVisibility!: ElementRef;
   @ViewChild('coursesButtonContainer') coursesButtonContainer!: ElementRef;
-  courses: string[] = ['LOG2410', 'LOG2420'];
+  @ViewChild('courseSearchInput') courseSearchInput!: ElementRef;
+  chosenCourses: string[] = [];
+  sortedSuggestions: string[] = [];
+  courseCode: string = '';
   date: Date = new Date();
   isPasswordVisible: boolean = false;
   isCoursesVisible: boolean = false;
-  coursesHeight: number = 35;
+  coursesHeight: number = 50;
 
-  constructor(public clientSocketService: ClientSocketService) {
+  constructor(
+    public clientSocketService: ClientSocketService,
+    private snackBar: MatSnackBar
+  ) {
     this.getList();
+    this.sortedSuggestions = this.clientSocketService.courses;
   }
 
   submit(): void {
@@ -37,19 +44,18 @@ export class HomePageComponent {
     user.password = this.passwordInput.nativeElement.value;
     user.dob = this.dobInput.nativeElement.value.replaceAll('-', '');
     user.email = this.emailInput.nativeElement.value;
-    user.courses = this.coursesInput.nativeElement.value;
+    user.courses = this.chosenCourses.toString();
+    if (
+      user.username.length < 0 ||
+      user.password.length < 0 ||
+      user.dob.length < 0 ||
+      user.email.length < 0 ||
+      user.dob.length < 0 ||
+      user.courses.length < 0
+    )
+      return;
 
-    // if (
-    //   user.username.length < 0 ||
-    //   user.password.length < 0 ||
-    //   user.dob.length < 0 ||
-    //   user.email.length < 0 ||
-    //   user.dob.length < 0 ||
-    //   user.courses.length < 0
-    // )
-    //   return;
-
-    // this.clientSocketService.sendDataToServer(user);
+    this.clientSocketService.sendDataToServer(user);
   }
 
   getList(): void {
@@ -73,23 +79,74 @@ export class HomePageComponent {
     const target = (event.target || event.currentTarget) as Element;
     if (target.id === 'course-name' || target.id === 'remove-course-btn')
       return;
+
+    if (this.isCoursesVisible) {
+      this.coursesButtonContainer.nativeElement.style.borderBottom =
+        '1px solid black';
+      this.coursesButtonContainer.nativeElement.style.borderBottomLeftRadius =
+        '3px';
+      this.coursesButtonContainer.nativeElement.style.borderBottomRightRadius =
+        '3px';
+    } else {
+      this.coursesButtonContainer.nativeElement.style.borderBottom = '0px';
+      this.coursesButtonContainer.nativeElement.style.borderBottomLeftRadius =
+        '0px';
+      this.coursesButtonContainer.nativeElement.style.borderBottomRightRadius =
+        '0px';
+    }
+
+    setTimeout(() => {
+      if (this.courseSearchInput) this.courseSearchInput.nativeElement.focus();
+    }, 100);
+
     this.isCoursesVisible = !this.isCoursesVisible;
+    if (this.courseSearchInput) {
+      this.courseSearchInput.nativeElement.value = '';
+      this.courseCode = '';
+      this.sortCoursesList('');
+    }
   }
 
   removeCourse(index: number): void {
-    this.courses.splice(index, 1);
+    this.chosenCourses.splice(index, 1);
     this.adjustHeight();
   }
 
   selectCourse(index: number): void {
-    const courseCode = this.clientSocketService.courses[index].split(' ')[0];
-    if (this.courses.includes(courseCode)) return;
-    this.courses.push(this.clientSocketService.courses[index].split(' ')[0]);
+    const courseCode = this.sortedSuggestions[index].split(' ')[0];
+    if (this.chosenCourses.includes(courseCode)) {
+      this.snackBar.open(courseCode + ' est déjà sélectionné', 'OK', {
+        duration: 2500,
+        verticalPosition: 'bottom',
+        panelClass: ['snack-bar'],
+      });
+      return;
+    } else if (this.chosenCourses.length >= 8) {
+      this.snackBar.open('Nombre de cours maximal atteint.', 'OK', {
+        duration: 2500,
+        verticalPosition: 'bottom',
+        panelClass: ['snack-bar'],
+      });
+      return;
+    }
+    this.chosenCourses.push(this.sortedSuggestions[index].split(' ')[0]);
     this.adjustHeight();
   }
 
-  adjustHeight():void{
-    const rows = Math.ceil(this.courses.length / 3);
-    this.coursesHeight = 35 * rows + ((rows - 1) * 5);
+  adjustHeight(): void {
+    const rows = Math.ceil(this.chosenCourses.length / 4);
+    this.coursesHeight = 50 * rows + (rows - 1) * 7.5;
+  }
+
+  sortCoursesList(courseCode: string): void {
+    this.sortedSuggestions = [];
+    if (courseCode.length === 0)
+      this.sortedSuggestions = this.clientSocketService.courses;
+    else {
+      for (const course of this.clientSocketService.courses) {
+        if (course.startsWith(courseCode.toUpperCase()))
+          this.sortedSuggestions.push(course);
+      }
+    }
   }
 }
